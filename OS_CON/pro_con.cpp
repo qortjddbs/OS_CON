9595f9b19a96189bff9167a974ba063ce036c33f
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-04-12 01:25:55 +0900

    7장까지 모두 끝

--- OS_CON/pro_con.cpp
+++ OS_CON/pro_con.cpp
@@ -2,12 +2,15 @@
 #include <thread>
 #include <queue>
 #include <mutex>
+#include <concurrent_queue.h>
+#include <chrono>
 
 constexpr long long LOOP = 100'0000;
 constexpr long long SUM = LOOP * (LOOP - 1) / 2;
 
 std::queue<int> buffer;
 std::atomic<long long> sum;
+concurrency::concurrent_queue<int> con_buffer;
 
 void producer()
 {
@@ -62,10 +65,26 @@ void a_consumer()
 	}
 }
 
+void con_producer()
+{
+	for (int i = 0; i < LOOP; ++i) con_buffer.push(i);
+}
+
+void con_consumer()
+{
+	for (int i = 0; i < LOOP; ++i) {
+		int val;
+		while (false == con_buffer.try_pop(val));	// 블러킹 알고리즘이지만 그냥 구현
+		sum += val;
+	}
+}
+
 int main()
 {
+	using namespace std::chrono;
 	{
 		std::cout << "Producer - Consumer without synchronization\n";
+		auto start_t = high_resolution_clock::now();
 		sum = 0;
 		// Producer #1
 		for (int i = 0;i < LOOP; ++i) buffer.push(i);
@@ -81,6 +100,9 @@ int main()
 			sum = sum + buffer.front();
 			buffer.pop();
 		}
+		auto end_t = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(end_t - start_t).count();
+		std::cout << "Duration : " << duration << "ms\n";
 
 		if (sum == SUM * 2)
 			std::cout << "Correct : " << sum << std::endl;
@@ -90,6 +112,7 @@ int main()
 
 	{
 		std::cout << "\nMulti Thread Producer - Consumer\n";
+		auto start_t = high_resolution_clock::now();
 		sum = 0;
 		std::thread p1{ a_producer };
 		std::thread p2{ a_producer };
@@ -99,6 +122,31 @@ int main()
 		p2.join();
 		c1.join();
 		c2.join();
+		auto end_t = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(end_t - start_t).count();
+		std::cout << "Duration : " << duration << "ms\n";
+
+		if (sum == SUM * 2)
+			std::cout << "Correct : " << sum << std::endl;
+		else
+			std::cout << "InCorrect : " << sum << std::endl;
+	}
+
+	{
+		std::cout << "\nConcurrent Queue Producer - Consumer\n";
+		auto start_t = high_resolution_clock::now();
+		sum = 0;
+		std::thread p1{ con_producer };
+		std::thread p2{ con_producer };
+		std::thread c1{ con_consumer };
+		std::thread c2{ con_consumer };
+		p1.join();
+		p2.join();
+		c1.join();
+		c2.join();
+		auto end_t = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(end_t - start_t).count();
+		std::cout << "Duration : " << duration << "ms\n";
 
 		if (sum == SUM * 2)
 			std::cout << "Correct : " << sum << std::endl;
